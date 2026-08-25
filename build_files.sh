7e6390aad727eb96e577236a4ab8a53a95ec4e13
Abhisheksaini25 <abhishek0saini7@gmail.com>
2026-08-25 17:35:39 +0530

    css

--- build_files.sh
+++ build_files.sh
@@ -4,3 +4,7 @@ python3 -m pip install -r requirements.txt --break-system-packages
 
 echo "Collecting static files..."
 python3 manage.py collectstatic --noinput --clear
+
+echo "Moving static files for Vercel CDN..."
+mkdir -p staticfiles_build/static
+mv staticfiles/* staticfiles_build/static/
